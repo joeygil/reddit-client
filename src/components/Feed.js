d5262d66b5ae9d2b70357e3a414d78cfa135924a
joeygil <105308164+joeygil@users.noreply.github.com>
2022-10-20 16:03:58 +0100

    final commit

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,6 +4,7 @@ import "../App.css";
 const Feed = () => {
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
+  const [popular, setPopular] = useState([]);
 
   const redditUrl = "https://www.reddit.com";
 
@@ -19,6 +20,14 @@ const Feed = () => {
       .then((data) => setData(data.data.children));
   };
 
+  const getPopular = async () => {
+    const response = await fetch("https://www.reddit.com/r/popular.json")
+      .then((data) => data.json())
+      .then((data) => setPopular(data.data.children));
+  };
+
+  console.log(popular);
+
   return (
     <>
       <div className="m-auto d-flex align-items-center justify-content-center  m-5">
@@ -29,31 +38,85 @@ const Feed = () => {
           onChange={(e) => setQuery(e.target.value)}
         />
         &nbsp;
-        <input type="submit" className="m-3 btn btn-dark" onClick={getData} />
+        <input
+          type="submit"
+          value="SEARCH"
+          className="m-3 btn btn-dark"
+          onClick={getData}
+        />
+        <div id="top-posts">
+          <button className="m-3 btn btn-dark" onClick={getPopular}>
+            GET TOP POSTS
+          </button>
+        </div>
       </div>
-      <div className="h-100 w-50 m-auto d-flex align-items-center justify-content-center border">
+      <div className="h-100 w-50 m-auto d-flex align-items-center justify-content-center">
         <br />
+
+        <div id="display">
+          {popular.map((post) => (
+            <>
+              <a
+                target="_blank"
+                className="h4 text-decoration-none text-dark"
+                href={`${redditUrl}${post.data.permalink}`}
+              >
+                <div key={post.data.id} id="post">
+                  <div id="author">Posted by: {post.data.author} </div>
+                  <div
+                    id="subreddit"
+                    className="h6 text-dark"
+                    key={post.data.id}
+                  >
+                    {post.data.subreddit_name_prefixed}&nbsp;
+                  </div>
+
+                  <div id="title">{post.data.title}</div>
+                  <div id="image" className="m-2">
+                    {post.data.url.includes(".png") ||
+                    post.data.url.includes("jpg") ? (
+                      <img src={post.data.url} alt="no picture" />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+              </a>
+            </>
+          ))}
+        </div>
+      </div>
+
+      <div className="h-100 w-50 m-auto d-flex align-items-center justify-content-center">
+        <br />
+
         <div id="display">
           {data.map((post) => (
             <>
-              <div id="post">
-                <div id="subreddit" key={post.data.id}>
-                  {post.data.subreddit_name_prefixed}&nbsp;
+              <a
+                target="_blank"
+                className="h4 text-decoration-none text-dark"
+                href={`${redditUrl}${post.data.permalink}`}
+              >
+                <div key={post.data.id} id="post">
+                  <div
+                    className="h6 text-dark"
+                    id="subreddit"
+                    key={post.data.id}
+                  >
+                    {post.data.subreddit_name_prefixed}&nbsp;
+                  </div>
+                  <div id="title">{post.data.title}</div>
+                  <div id="image">
+                    {post.data.url.includes(".png") ||
+                    post.data.url.includes("jpg") ? (
+                      <img src={post.data.url} alt="no picture" />
+                    ) : (
+                      ""
+                    )}
+                  </div>
                 </div>
-                <div id="title">
-                  <a href={`${redditUrl}${post.data.permalink}`}>
-                    {post.data.title}
-                  </a>
-                </div>
-                <div id="image">
-                  {post.data.url.includes(".png") ||
-                  post.data.url.includes("jpg") ? (
-                    <img src={post.data.url} alt="no picture" />
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </div>
+              </a>
             </>
           ))}
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import { faReddit } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
 const Header = () => {
-
- 
   return (
     <>
       <div className="d-flex justify-content-around header h1 bg-secondary text-white">
         <div className="m-3">
-          <FontAwesomeIcon icon={faReddit} />
-          &nbsp;REDDIT
+          <FontAwesomeIcon icon={faReddit} /> &nbsp;
+          
         </div>
       </div>
     </>
